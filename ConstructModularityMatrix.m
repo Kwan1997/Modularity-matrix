fab8261b16bbd05ff739bddbcf8d222625e9755e
gjw <23220191151283@stu.xmu.edu.cn>
2021-03-28 21:30:38 +0800

    debug

--- ConstructModularityMatrix.m
+++ ConstructModularityMatrix.m
@@ -1,6 +1,6 @@
-function [B, B1] = ConstructModularityMatrix(A)
+function [B, B2] = ConstructModularityMatrix(A)
     deg = sum(A, 2);
     m = 0.5 * sum(deg);
-    B1 = (deg * deg') ./ (2 * m);
-    B = A - B1;
+    B2 = (deg * deg') ./ (2 * m);
+    B = A - B2;
 end
